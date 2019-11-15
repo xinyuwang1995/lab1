ced12922f0a721888b6b6dcab40aff9e49163cc2
xinyuwang1995 <wangxinyu19950915@gmail.com>
2019-11-15 19:57:12 +0800

    pytest

--- apps/account/test_account.py
+++ apps/account/test_account.py
@@ -5,7 +5,12 @@ class ModelTest(TestCase):
 
     def setUp(self):
         MyUser.objects.create(username='test',nickname='test_ac',gender='male',avatar='https://pic1.zhimg.com/v2-fda399250493e674f2152c581490d6eb_1200x500.jpg')
-
+        MyUser.objects.create(nickname='test_gic',gender='female',avatar='https://pic1.zhimg.com/v2-fda399250493e674f2152c581490d6eb_1200x500.jpg')
+        GithubUser.objects.create(name='gi_test',email='test@test.com',bio='test_h',user_id=2)
     def test_user_register(self):
         res = MyUser.objects.get(username='test')
         self.assertEqual(res.gender,"male")
+
+    def test_githubuser_register(self):
+        res = GithubUser.objects.get(name='gi_test')
+        self.assertEqual(res.email,'test@test.com')
--- apps/product/test_product.py
+++ apps/product/test_product.py
@@ -3,10 +3,12 @@ from  .models import Product
 
 class ModelTest(TestCase):
     def setUp(self):
-       Product.objects.create(pid='1',name='yandex',url='http://www.yandex.ru',digest='search engine',
+        Product.objects.create(pid='1',name='yandex',url='http://www.yandex.ru',digest='search engine',
                               user_id=1)
-
-
+        Product.objects.create(pid='2',name='facebook',url='http://www.facebook.com',digest='social',user_id=2)
     def test_product_register(self):
         res = Product.objects.get(name='yandex')
         self.assertEqual(res.url,"http://www.yandex.ru")
+    def test_product_gituser_register(self):
+        res = Product.objects.get(name='facebook')
+        self.assertEqual(res.url,'http://www.facebook.com')
